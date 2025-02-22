5f2c2b444454bba32bf34995df0c17212123b5be
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-22 11:56:24 +0530

    [Avinash] Added Service Layer for Greeting Message using Spring boot of my project Greeting App

--- GreetingApp/GreetingApp/src/main/java/com/example/greetingApp/controller/GreetingController.java
+++ GreetingApp/GreetingApp/src/main/java/com/example/greetingApp/controller/GreetingController.java
@@ -1,14 +1,23 @@
 package com.example.greetingApp.controller;
 
+import com.example.greetingApp.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    // GET Request - Default Greeting
+    private final GreetingService greetingService;
+
+    // Constructor Injection
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    // GET Request - Using Service Layer
     @GetMapping
     public String getGreeting() {
-        return "{ \"message\": \"Hello, Welcome to the Greeting App Using SpringBoot!\" }";
+        return "{ \"message\": \"" + greetingService.getGreetingMessage() + "\" }";
     }
 }
+
